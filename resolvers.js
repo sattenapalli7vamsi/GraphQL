3b6cb7811a35f4d69f55ec49564a6394bc5414dc
Vamsi Sattenapalli <sattenapalli7vamsi@gmail.com>
2024-10-24 16:31:56 -0400

    Add files via upload

--- resolvers.js
+++ resolvers.js
@@ -1,19 +1,17 @@
 // src/resolvers.js
 const users = [
-  { id: '1', name: 'John Doe', email: 'john@example.com' },
-  { id: '2', name: 'Jane Smith', email: 'jane@example.com' },
-];
+    { id: '1', name: 'John Doe', email: 'john@example.com' },
+    { id: '2', name: 'Jane Smith', email: 'jane@example.com' },
+  ];
 
 const resolvers = {
   // RootQuery resolver
-  RootQueryType: {
-    users: () => users, // Return all users from the defined array
-    user: (parent, args) => users.find(user => user.id === args.id), // Find user by ID
-  },
-  
-  // UserType resolvers (if needed for fields)
-  UserType: {
-    // If you need custom logic for fields, define them here
+  RootQuery: {
+    users: (parent, args) => users, // Return all users from the defined array
+    user: (parent, args) => {if (args.id && args.name) {return users.find(user => user.id === args.id && user.name === args.name)}
+                             if (args.id){return users.find(user => user.id === args.id)}
+                             if (args.name){return users.find(user => user.name == args.name)}
+   } // Find user by ID
   },
 };
 
--- schema.js
+++ schema.js
@@ -1,42 +1,54 @@
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList } = require('graphql');
-
+const { GraphQLNonNull, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList } = require('graphql');
 
 // Define User type
-const UserType = new GraphQLObjectType({
+const User = new GraphQLObjectType({
   name: 'UserType',
   description: "This is UserType",
   fields: () => ({
     id: { type: GraphQLString, description: "id of the user" },
     name: { type: GraphQLString },
     email: { type: GraphQLString },
+    // fullName: {
+    //     type: GraphQLString,
+    //     args: {
+    //       uppercase: { type: GraphQLBoolean } // Accept an argument to format the name
+    //     },
+    //     resolve(parent, args) {
+    //       if (args.uppercase) {
+    //         return parent.name.toUpperCase();
+    //       }
+    //       return parent.name;
+    //     }
+    //   },
+    //
+    //uncomment the above lines, if you want to fetch the name of the user in upper case
   }),
   
 });
 
+
 // Define RootQuery
-const RootQueryType = new GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   description: "This is RootQuery",
   fields: {
     users: {
-      type: new GraphQLList(UserType), // Return a list of users
-    //   resolve(parent, args) {
-    //     return users; // Return the sample data
-    //  },
+      type: new GraphQLList(User),
+      description: "This gives all the users' details" // Return a list of users
     },
     user: {
-      type: UserType,
-      args: { id: { type: GraphQLString } }, // Argument for the user ID
-    //   resolve(parent, args) {
-    //     return users.find(user => user.id === args.id); // Find user by ID
-    //   },
+      type: User,
+      description: " This gives details of a user",
+      args: { id: { type: new GraphQLNonNull(GraphQLString)},  // add default value -->  defaultValue: '1', it bypasses the condition of Non-Null
+              name: {type: GraphQLString} }, // Argument for the user ID
     },
   },
 });
 
+
 // Create Schema
 const schema = new GraphQLSchema({
-  query: RootQueryType,
+  query: RootQuery,
 });
 
 module.exports = schema;
